68ac69a8c212b1f7833c04e00cc0b97a88bab59d
Alisa Gorislav <61160686+alyferryhalo@users.noreply.github.com>
2021-03-12 08:09:12 +0500

    Create task_53.c

---
+++ 2_homework/task_53.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  float a;
+  int n;
+  int k;
+  float s = 1.0;
+  // считать a, n
+  // повторять:
+  // s = s * (a+k)
+  // k = k + 1
+  // до тех пор, пока k = n
+  // вывести s
+  return 0;
+}
