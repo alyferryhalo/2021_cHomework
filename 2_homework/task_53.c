d8db9905fd27005540eac6e5f9299175ad88b4f2
Alisa Gorislav <61160686+alyferryhalo@users.noreply.github.com>
2021-03-20 15:50:20 +0500

    Update task_53.c

--- 2_homework/task_53.c
+++ 2_homework/task_53.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 
 int main(){
-  float a;
-  int n;
+  int a; // начальное значение
+  int n; // количество шагов
   int k;
-  float s = 1.0;
+  int s = 1; // итоговая сумма
   // считать a, n
+  scanf("%d",&a);
+  scanf("%d",&n);
   // повторять:
   // s = s * (a+k)
   // k = k + 1
